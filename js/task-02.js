d2cd04958c42d6792ea6c223b82f351d39ac83e4
Sergii Marchenko <somarchenko@gmail.com>
2023-03-20 22:52:34 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.getElementById('ingredients');
+
+const allItems = [];
+
+const markup = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  allItems.push(item);
+});
+
+list.append(...allItems);
