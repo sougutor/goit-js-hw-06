be8149e3263fc0259b2d0d09dd2991a1a571fa1d
Sergii Marchenko <somarchenko@gmail.com>
2023-03-22 15:21:20 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,13 @@
+const refs = {
+  text: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+};
+
+refs.btn.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  refs.text.textContent = document.body.style.backgroundColor;
 }
