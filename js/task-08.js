74948982f2070d31ec07b64d1980dba3a7fcf2af
Sergii Marchenko <somarchenko@gmail.com>
2023-03-22 15:21:15 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+  inputs: document.querySelectorAll('input'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (refs.inputs[0].value === '' || refs.inputs[1].value === '') {
+    alert(`Усі поля повинні бути заповнені!`);
+    return;
+  }
+  const form = event.currentTarget.elements;
+  const valueForm = {
+    mail: form.email.value,
+    password: form.password.value,
+  };
+  console.log(valueForm);
+  event.currentTarget.reset();
+}
